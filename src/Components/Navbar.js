b87d7613843f991175a27dcaebcd217293df3502
unknown <kshykumr@gmail.com>
2023-12-09 17:55:17 +0530

    front-end-completed

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -12,11 +12,11 @@ function Navbar() {
     <div className='Navbar-box1'>
       <div className='Navbar-box1-box'>
      <div style={{height:'35px',width:'100%',borderRadius:'7px',display:'flex',flexDirection:'row',justifyContent:'center',alignItems:'center',border:'1px solid #B0B0B0',margin:'0px 6px',padding:'2px 5px'}}>
-      <div style={{height:'25px',width:'20%',margin:'0px 10px 0px 0px'}}><img src={SearchImage} alt='search' /></div>
+      <div style={{height:'25px',width:'20%',margin:'0px 10px 0px 0px'}}><img src={SearchImage}style={{height:'20px'}} alt='search' /></div>
       <input type='text' style={{height:'28px',border:'none'}}/>
       </div> 
       <Link to='/cart' style={{textDecoration:'none',color:'#757575'}}><div style={{height:'30px',width:'30px',margin:'0px 6px'}}><img src={CartImage} alt='cart' /></div></Link>
-      <div style={{height:'30px',width:'30px',margin:'0px 6px'}}><img src={HeartImage} alt='heart' /></div>
+      <Link to='/wishlist' style={{textDecoration:'none',color:'#757575'}}><div style={{height:'30px',width:'30px',margin:'0px 6px'}}><img src={HeartImage} alt='wishlist' /></div></Link>
       <Link to='/profile' style={{textDecoration:'none',color:'#757575'}}><div style={{height:'30px',width:'30px',margin:'0px 6px'}}><img src={ProfileImage} alt='profile' /></div></Link>
       </div>
     </div>
---
+++ src/Pages/About/About.js
@@ -0,0 +1,33 @@
+import './About.css';
+import SideImage from '../../Images/SDP05271.png'
+function About() {
+  return (
+    <section className='Login' style={{width:'100%',height:'auto',margin:'50px 0px',display:'flex',justifyContent:'center',alignItems:'center',flexDirection:'column'}}>
+      <div style={{width:'100%',display:'flex',justifyContent:'center',alignItems:'center',flexDirection:'column',height:'100%'}}>
+    <div style={{display:'flex',justifyContent:'center',alignItems:'center',flexDirection:'row',width:'95%',height:'100%',borderBottom:'1px solid #BD9334',padding:'52px 0px'}}>
+        <div style={{width:'50%',height:'567px',margin:'0px 18px'}}><img src={SideImage} alt='SideImage' style={{height:'100%',width:'100%'}}/></div>
+        <div style={{width:'50%',display:'flex',justifyContent:'flex-start',alignItems:'center',flexDirection:'column',height:'100%'}}>
+          <div style={{fontFamily:'Abhaya Libre',fontWeight:'400',fontSize:'30px',color:'#BD9334',margin:'0px 0px 10px 0px'}}>~We are Rantika Thakur Clothing ~</div>
+          <div style={{fontFamily:'Abhaya Libre',fontWeight:'400',fontSize:'24px',display:'flex',justifyContent:'space-between',alignItems:'center',flexDirection:'column',width:'100%',textAlign:'left'}}>
+           Rantika Thakur Clothing was established on 2022 by our founder Rantika wanting to create a brand for girls just like her. Designer clothes were always so pricey, and she felt a strong desire to change that.<br/><br/> That's why she started her own fashion brand, bringing the latest runway trends to you at guilt-free prices. So, with rantika thakur clothing, you can now stay on trend and be the prettiest version of yourself without breaking the bank. We are all about making fashion fun, affordable, and accessible for everyone.<br/><br/> Now you can enjoy the latest fashion without feeling guilty about the price tag, ensuring you look your best whenever you want!
+          </div>
+        </div>
+    </div>
+    </div>
+
+
+    <div style={{display:'flex',justifyContent:'center',alignItems:'center',flexDirection:'row',width:'90%',height:'100%',borderBottom:'1px solid #BD9334',padding:'52px 0px'}}>
+        <div style={{width:'100%',display:'flex',justifyContent:'flex-start',alignItems:'center',flexDirection:'column',height:'100%'}}>
+          <div style={{fontFamily:'Abhaya Libre',fontWeight:'400',fontSize:'30px',color:'#BD9334',margin:'0px 0px 10px 0px'}}>~About The founder~</div>
+          <div style={{width:'100%',height:'500px',margin:'50px 18px'}}><img src={SideImage} alt='SideImage' style={{height:'100%',width:'100%'}}/></div>
+          <div style={{fontFamily:'Abhaya Libre',fontWeight:'400',fontSize:'24px',display:'flex',justifyContent:'space-between',alignItems:'center',flexDirection:'column',width:'100%',textAlign:'left'}}>
+          Founded by Rantika Thakur, who's a blogger turned entrepreneur.<br/><br/> Professionally Rantika started her career in fashion at 18, as a blogger while pursuing her degree in fashion. Soon after completing her Masters in fashion Rantika Thakur Clothing was born.<br/><br/>
+          </div>
+        </div>
+    </div>
+    </section>
+  );
+}
+
+export default About;
+
