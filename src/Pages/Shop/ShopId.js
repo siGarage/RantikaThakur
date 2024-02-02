9416fc872e3fc528890376b280e75b8177e2e2ed
kartik <kartik65361@gmail.com>
2024-02-02 23:21:44 +0530

    small change

--- src/Pages/Shop/ShopId.js
+++ src/Pages/Shop/ShopId.js
@@ -409,7 +409,7 @@ function ShopId(props) {
             <div className="col-6"></div>
             <div className="ProductDescriptionBox2-ProductBox">
               <h5 style={{ margin: "46px 0px" }}>Product Description</h5>
-              <div style={{ fontSize: "16px", }}>
+              <div style={{ fontSize: "15px", fontFamily:'sans-serif'}}>
               {parse(
                 `<ul>
 
